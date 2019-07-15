07d2d80060a154542c3e69aff11e8a7920ddad5d
José Luís Oliveira da Cunha <joseloc300-dev@outlook.com>
2019-07-15 03:23:01 +0100

    create logical device and queue
    following vulkan-tutorial.com. end of sub-chapter "logical device and queues"

--- main.cpp
+++ main.cpp
@@ -36,6 +36,7 @@ class HelloTriangleApplication {
 		VkDebugUtilsMessengerEXT debugMessenger;
 
 		VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+		VkDevice device;		VkQueue graphicsQueue;
 
 		const std::vector<const char*> validationLayers = {
 			"VK_LAYER_KHRONOS_validation"
@@ -72,6 +73,45 @@ class HelloTriangleApplication {
 			createInstance();
 			setupDebugMessenger();
 			pickPhysicalDevice();
+			createLogicalDevice();
+		}
+
+		void createLogicalDevice() {
+			QueueFamilyIndices indices = findQueueFamilies(physicalDevice);
+
+			VkDeviceQueueCreateInfo queueCreateInfo = {};
+			queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+			queueCreateInfo.queueFamilyIndex = indices.graphicsFamily.value();
+			queueCreateInfo.queueCount = 1;
+
+			float queuePriority = 1.0f;
+			queueCreateInfo.pQueuePriorities = &queuePriority;
+
+			VkPhysicalDeviceFeatures deviceFeatures = {};
+
+			VkDeviceCreateInfo createInfo = {};
+			createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+
+			createInfo.pQueueCreateInfos = &queueCreateInfo;
+			createInfo.queueCreateInfoCount = 1;
+
+			createInfo.pEnabledFeatures = &deviceFeatures;
+
+			createInfo.enabledExtensionCount = 0;
+
+			if (enableValidationLayers) {
+				createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+				createInfo.ppEnabledLayerNames = validationLayers.data();
+			}
+			else {
+				createInfo.enabledLayerCount = 0;
+			}
+
+			if (vkCreateDevice(physicalDevice, &createInfo, nullptr, &device) != VK_SUCCESS) {
+				throw std::runtime_error("failed to create logical device");
+			}
+
+			vkGetDeviceQueue(device, indices.graphicsFamily.value(), 0, &graphicsQueue);
 		}
 
 		void pickPhysicalDevice() {
@@ -326,10 +366,9 @@ class HelloTriangleApplication {
 				DestroyDebugUtilsMessengerEXT(instance, debugMessenger, nullptr);
 			}
 
+			vkDestroyDevice(device, nullptr);
 			vkDestroyInstance(instance, nullptr);
-
 			glfwDestroyWindow(window);
-
 			glfwTerminate();
 		}
 
