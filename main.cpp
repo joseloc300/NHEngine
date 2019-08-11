27e7cd0e47b6e24af0ae1387d4ae73e241ea6c0e
José Luís Oliveira da Cunha <joseloc300-dev@outlook.com>
2019-08-11 16:50:08 +0100

    create image views
    following vulkan-tutorial.com. end of sub-chapter "image views"

--- main.cpp
+++ main.cpp
@@ -38,6 +38,7 @@ class HelloTriangleApplication {
 
 		VkSwapchainKHR swapChain;
 		std::vector<VkImage> swapChainImages;
+		std::vector<VkImageView> swapChainImageViews;
 		VkFormat swapChainImageFormat;
 		VkExtent2D swapChainExtent;
 
@@ -98,6 +99,34 @@ class HelloTriangleApplication {
 			pickPhysicalDevice();
 			createLogicalDevice();
 			createSwapChain();
+			createImageViews();
+		}
+
+		void createImageViews() {
+			swapChainImageViews.resize(swapChainImages.size());
+
+			for (size_t i = 0; i < swapChainImages.size(); i++) {
+				VkImageViewCreateInfo createInfo = {};
+				createInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+				createInfo.format = swapChainImageFormat;
+
+				createInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+				createInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+				createInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+				createInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
+
+				createInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+				createInfo.subresourceRange.baseMipLevel = 0;
+				createInfo.subresourceRange.levelCount = 1;
+				createInfo.subresourceRange.baseArrayLayer = 0;
+				createInfo.subresourceRange.layerCount = 1;
+
+				if (vkCreateImageView(device, &createInfo, nullptr, &swapChainImageViews[i]) != VK_SUCCESS) {
+					throw std::runtime_error("failed to create image views");
+				}
+
+			}
+
 		}
 
 		void createSwapChain() {
@@ -550,6 +579,10 @@ class HelloTriangleApplication {
 		void cleanup() {
 			if (enableValidationLayers) {
 				DestroyDebugUtilsMessengerEXT(instance, debugMessenger, nullptr);
+			}
+
+			for (auto imageView : swapChainImageViews) {
+				vkDestroyImageView(device, imageView, nullptr);
 			}
 
 			vkDestroySwapchainKHR(device, swapChain, nullptr);
